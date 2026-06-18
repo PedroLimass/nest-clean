0e9edcf26010bde87827ccb9e92394f233f8d76b
Pedro Lima <phlimas@outlook.com>
2026-06-17 22:39:35 -0300

    feat: add create account controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+    }),
+    PrismaModule,
+  ],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Env } from './env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+    // cors: true,
+  });
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = configService.get('PORT', { infer: true });
+
+  await app.listen(port);
+  console.log(`Server is running on port ${port}`);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
