9d4835e63c29fa49fa941d7b47caddde84a2665b
Pedro Lima <phlimas@outlook.com>
2026-06-18 15:47:56 -0300

    feat: add authentication and authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,24 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validate: (env) => envSchema.parse(env),
     }),
     PrismaModule,
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
