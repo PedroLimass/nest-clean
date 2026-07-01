f053005100c1e9587be86a4d5c8796b7cefa5269
Pedro Lima <phlimas@outlook.com>
2026-07-01 11:45:06 -0300

    feat: add controllers for choose question best answer, comment on question, delete question comment, fetch question answers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -22,6 +22,14 @@ import { EditAnswerController } from './controllers/edit-answer.controller';
 import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer';
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer';
 import { DeleteAnswerController } from './controllers/delete-answer.controller';
+import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller';
+import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers';
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller';
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer';
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller';
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question';
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment';
+import { DeleteQuestionCommentController } from './controllers/delete-question-comment.controller';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -36,6 +44,10 @@ import { DeleteAnswerController } from './controllers/delete-answer.controller';
     AnswerQuestionController,
     EditAnswerController,
     DeleteAnswerController,
+    FetchQuestionAnswersController,
+    ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
+    DeleteQuestionCommentController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -48,6 +60,10 @@ import { DeleteAnswerController } from './controllers/delete-answer.controller';
     AnswerQuestionUseCase,
     EditAnswerUseCase,
     DeleteAnswerUseCase,
+    FetchQuestionAnswersUseCase,
+    ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
+    DeleteQuestionCommentUseCase,
   ],
 })
 export class HttpModule {}
